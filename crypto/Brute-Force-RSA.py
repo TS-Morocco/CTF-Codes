14b1d2af9b97cfcf1ae234f28b9e46877b805cd5
Souf-Source <78572574+Souf-Source@users.noreply.github.com>
2021-07-28 19:43:08 +0100

    Create Brute-Force-RSA.py

---
+++ crypto/Brute-Force-RSA.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import string
+def main():
+    flag = ''
+    e = 65537
+    n = 28...
+    encrypted_chars = [2876876...]
+    for encrypted_char in encrypted_chars:
+        for char in string_printable:
+            if encrypted_char == pow(ord(char), e, n):
+                flag += char
+    print(flag)
+    
+if __name__ == '__main__':
+    main()
+    
