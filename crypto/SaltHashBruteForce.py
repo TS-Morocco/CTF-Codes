2a24f209e3443cf9be87f9c94abe5e907c134ed4
Souf-Source <78572574+Souf-Source@users.noreply.github.com>
2021-08-29 15:07:23 +0100

    Create SaltHashBruteForce.py
    A simple script used in previous CTFs to brute force the Hash of a password using salt.

---
+++ crypto/SaltHashBruteForce.py
@@ -0,0 +1,17 @@
+import bcrypt
+import base64
+
+salt   = b'$2XXXXXXXXXXXXXXXXXXXX'
+hashPass = b'$2XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+
+with open("rockyou.txt","r") as f:
+	for word in f.readlines():
+		passw = word.strip().encode('ascii', 'ignore')
+		b64str = base64.b64encode(passw)
+		hashAndSalt = bcrypt.hashpw(b64str, salt)
+		#print(hashAndSalt)
+		#print(passw)
+
+		if hashPass == hashAndSalt:
+			print("[+] Password Found!!!: %s" % passw)
+			break
