f6bb9095d2d56bcad546943436eb83590a851c10
GoCode <78572574+GoCode0@users.noreply.github.com>
2021-10-29 23:24:46 +0100

    Update Brute-Force-RSA.py

--- crypto/Brute-Force-RSA.py
+++ crypto/Brute-Force-RSA.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
+# Importing string library | Importation de la librarie "string"
 import string
 def main():
+    # Strarting with an empty flag | De`marrage avec un flag vide
     flag = ''
+    # Defining e n
     e = 65537
     n = 28...
+    # Defining Encrypted Message | Definition du message crypte`
     encrypted_chars = [2876876...]
+    # conditionnel double loop to print the flag
     for encrypted_char in encrypted_chars:
         for char in string.printable:
             if encrypted_char == pow(ord(char), e, n):
@@ -12,5 +17,4 @@ def main():
     print(flag)
     
 if __name__ == '__main__':
-    main()
-    
+    main()    
